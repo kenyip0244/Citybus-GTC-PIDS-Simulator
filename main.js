cae89b143d185e19cd3d01fa65d3be9efe4f7a68
kenyip0244 <your_email@gmail.com>
2026-08-18 11:53:50 +0800

    Initialize clean repository with GitHub Actions build workflow

---
+++ main.js
@@ -0,0 +1,30 @@
+﻿const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    fullscreen: true,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  Menu.setApplicationMenu(null);
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
